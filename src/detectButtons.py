54762bc41dd2d7f1d8a9f3c774ff38a7b258150c
Daniel Diamand <daniel@diamand.org>
2023-10-19 11:41:49 +0100

    Update detectButton with safer code

--- src/detectButtons.py
+++ src/detectButtons.py
@@ -4,17 +4,24 @@ import time
 GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
-#The GPIO pins connected to buttons
-connectedPins = [2, 3, 4, 17]
+def detectButtons():
+    #The GPIO pins connected to buttons
+    connectedPins = [2, 3, 4, 17]
 
-for pin in connectedPins:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    for pin in connectedPins:
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def button_pressed(pin):
-    print("Button connected to pin",pin,"was pressed!")
+    def button_pressed(pin):
+        print("Button connected to pin",pin,"was pressed!")
 
-for pin in connectedPins:
-    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_pressed, bouncetime=200)
+    for pin in connectedPins:
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_pressed, bouncetime=200)
 
-while True: #Let program keep running
-    time.sleep(0.1)  
+    try:
+        while True: #Let program keep running
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    detectButtons()
