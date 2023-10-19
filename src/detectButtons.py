4620851b94638e66b1b4a2b26cb72589dda19dc5
Daniel Diamand <daniel@diamand.org>
2023-10-19 11:20:14 +0100

    Added a sleep and fixed bug

--- src/detectButtons.py
+++ src/detectButtons.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 #The GPIO pins connected to buttons
 connectedPins = [2, 3, 4, 17]
@@ -13,3 +14,4 @@ while True:
     for pin in connectedPins:
         if GPIO.input(pin) == GPIO.LOW:
             print("Button connected to pin",pin,"was pressed!")
+            time.sleep(0.1)
