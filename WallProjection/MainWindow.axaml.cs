2382778412ae41991f9600af6feb4e8eb8661420
danield <daniel@diamand.org>
2023-10-19 14:42:34 +0100

    Add displays image

--- WallProjection/MainWindow.axaml.cs
+++ WallProjection/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using System.Drawing;
 
 namespace WallProjection;
 
@@ -7,5 +10,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        this.ShowImage("./assets/image1.jpg");
+    }
+
+    private void ShowImage(string path)
+    {
+        displayedImage.IsVisible = true;
+        var bitmap = new Avalonia.Media.Imaging.Bitmap(@"assets/image1.jpg");
+        displayedImage.Source = bitmap;
     }
 }
