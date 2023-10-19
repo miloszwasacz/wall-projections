b12c7ebbe2903aab44e083544716ae6adebd6927
Daniel Diamand <daniel@diamand.org>
2023-10-19 11:28:40 +0100

    Update detectButton to use events

--- src/detectButtons.py
+++ src/detectButtons.py
@@ -10,8 +10,11 @@ connectedPins = [2, 3, 4, 17]
 for pin in connectedPins:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    for pin in connectedPins:
-        if GPIO.input(pin) == GPIO.LOW:
-            print("Button connected to pin",pin,"was pressed!")
-            time.sleep(0.1)
+def button_pressed(pin):
+    print("Button connected to pin",pin,"was pressed!")
+
+for pin in connectedPins:
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_pressed, bouncetime=200)
+
+while True: #Let program keep running
+    time.sleep(0.1)  
