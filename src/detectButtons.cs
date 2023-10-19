b4fc646f531dd46e13db03005c55c46a80d8403a
danield <daniel@diamand.org>
2023-10-19 17:06:42 +0100

    Add detect input on multiple pins

--- src/detectButtons.cs
+++ src/detectButtons.cs
@@ -1,21 +1,23 @@
-//Based of learn.microsoft.com/en-us/dotnet/iot/tutorials/gpio-input
-
 using System.Device.Gpio;
 using System.Threading.Tasks;
 
-int pin = 21;
+int[] pins = { 2, 3, 4, 17 };
 
 using var controller = new GpioController();
-controller.OpenPin(pin, PinMode.InputPullUp);
 
-controller.RegisterCallbackForPinValueChangedEvent(
+foreach (int pin in pins)
+{
+    controller.OpenPin(pin, PinMode.InputPullUp); //Detecting change in voltage for each pin in pins
+    controller.RegisterCallbackForPinValueChangedEvent( //If pin is Falling (connected to GND) call function OnPinEvent
     pin,
     PinEventTypes.Falling,
-    OnPinEvent);
+    (sender, args) => OnPinEvent(sender, args, pin));
+}
 
-await Task.Delay(Timeout.Infinite);
+await Task.Delay(Timeout.Infinite); //Wait infinitly to stop code terminating
 
-static void OnPinEvent(object sender, PinValueChangedEventArgs args)
+static void OnPinEvent(object sender, PinValueChangedEventArgs args, int pin)
 {
-    Console.WriteLine("Pressed!");
+    Console.WriteLine("Pressed pin:" + pin);
+    Task.Delay(500).Wait();
 }
