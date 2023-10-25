5886ab7e2c63a66e64c1aae877710490704998c3
danield <daniel@diamand.org>
2023-10-25 14:06:04 +0100

    Add video working but in different window

--- WallProjection/MainWindow.axaml.cs
+++ WallProjection/MainWindow.axaml.cs
@@ -13,14 +13,17 @@ namespace WallProjection;
 public partial class MainWindow : Window
 {
 
-    /*private readonly LibVLC _libVlc = new();*/
+    private readonly LibVLC _libVlc = new();
 
     public MainWindow()
     {
         InitializeComponent();
         displayedImage = this.Find<Avalonia.Controls.Image>("displayedImage");
         displayedVideo = this.Find<VideoView>("displayedVideo");
+        MediaPlayer = new MediaPlayer(_libVlc);
+        displayedVideo.MediaPlayer = MediaPlayer;
         this.KeyDown += KeyDownHandler;
+        ShowVideo("S:/uni/coding/2023-WallProjections/WallProjection/bin/x64/Debug/net6.0/assets/video1.mp4");
     }
 
     private void InitializeComponent()
@@ -41,7 +44,7 @@ public partial class MainWindow : Window
                 this.ShowImage("assets/image1.jpg");
                 break;
             case Key.B:
-                this.ShowImage("assets/image2.jpg");
+                this.ShowVideo("S:/uni/coding/2023-WallProjections/WallProjection/bin/x64/Debug/net6.0/assets/video1.mp4");
                 break;
         }
     }
@@ -51,6 +54,8 @@ public partial class MainWindow : Window
     {
         try
         {
+            displayedVideo.MediaPlayer.Stop();
+            displayedVideo.IsVisible = false;
             displayedImage.IsVisible = true;
             var bitmap = new Avalonia.Media.Imaging.Bitmap(path);
             displayedImage.Source = bitmap;
@@ -60,4 +65,24 @@ public partial class MainWindow : Window
             Console.Error.WriteLine($"Error displaying image at path {path}: {e.Message}");
         }
     }
+
+    private void ShowVideo(string path)
+    {
+        try
+        {
+            displayedImage.IsVisible = false;
+            displayedVideo.IsVisible = true;
+            using var media = new Media(
+                _libVlc,
+                new Uri(path)
+            );
+            displayedVideo.MediaPlayer.Play(media);
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine($"Error displaying image at path {path}: {e.Message}");
+        }
+    }
+
+    public MediaPlayer MediaPlayer { get; }
 }
