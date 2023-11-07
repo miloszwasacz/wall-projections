4a7e1e71e499ae1e7b7f3e8b246f4ca74dc3779b
Daniel Diamand <daniel@diamand.org>
2023-11-07 13:34:58 +0000

    Add respond to finger touching hotspot
    Co-authored-by: Sergi Lange-Soler <qq22886@bristol.ac.uk>

--- WallProjections/Scripts/main.py
+++ WallProjections/Scripts/main.py
@@ -1,28 +1,53 @@
-# This is the main entrypoint for C# to call into Python
-import RPi.GPIO as GPIO
-import time
+import cv2
+import mediapipe as mp
 
-GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BCM)
+hands_model = mp.solutions.hands.Hands()
+webcam = cv2.VideoCapture(0)
 
+HOTSPOT_COORDS = (0.5, 0.5)
+HOTSPOT_SCREEN_COORDS = (320, 240)
+FINGERTIP_INDICES = (4, 8, 12, 16, 20)
+
+hotspot_was_pressed = False
 
 def detect_buttons(event_handler): #This function is called by Program.cs
-    pinToButtonId = { #Matches GPIO pins on the Pi to the value passed to Program.cs
-        2: 1,
-        3: 2,
-        4: 3,
-        17: 4
-    }
+    while webcam.isOpened():
+        success, img = webcam.read()
 
-    def buttonPressed(id):
-        event_handler.OnPressDetected(id)
+        # run model
+        img_rbg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        model_output = hands_model.process(img_rbg)
 
-    for pin, id in pinToButtonId.items(): #Foreach pin
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup pin on Pi
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=buttonPressed(id), bouncetime=200) #If pin falls (buttons pressed) call buttonPressed function
+        # detect if finger over hotspot
+        if model_output.multi_hand_landmarks is not None:
+            hotspot_pressed = False
 
-    try:
-        while True: #Let program keep running
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+            for landmarks in model_output.multi_hand_landmarks:
+                for i in FINGERTIP_INDICES:
+                    fingertip_coords = landmarks.landmark[i]
+                    if (fingertip_coords.x - HOTSPOT_COORDS[0])**2 + (fingertip_coords.y - HOTSPOT_COORDS[1])**2 <= 0.001:
+                        hotspot_pressed = True
+                        if not hotspot_was_pressed:
+                            event_handler.OnPressDetected(1)
+
+            hotspot_was_pressed = hotspot_pressed
+
+        # annotate hand landmarks and hotspot onscreen
+        cv2.circle(img, HOTSPOT_SCREEN_COORDS, 10, (255, 0, 255), thickness=2)
+        if model_output.multi_hand_landmarks is not None:
+            for landmarks in model_output.multi_hand_landmarks:
+                mp.solutions.drawing_utils.draw_landmarks(img, landmarks, connections=mp.solutions.hands.HAND_CONNECTIONS)
+
+        cv2.imshow("Hand Tracking Testing", img)
+
+        if cv2.waitKey(5) & 0xFF == ord("q"):
+            break
+
+    webcam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    class EventHandler:
+        def OnPressDetected(self, id):
+            print(id)
+    detect_buttons(EventHandler())
