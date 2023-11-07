fad67667c45815036f1449aeb862fa21ac6d8302
Daniel Diamand <daniel@diamand.org>
2023-11-07 13:05:20 +0000

    Add more comments, improved readibilites

--- WallProjections/Scripts/main.py
+++ WallProjections/Scripts/main.py
@@ -6,10 +6,8 @@ GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BCM)
 
 
-def detect_buttons(event_handler):
-    
-
-    pinToButtonId = { #matches GPIO pins on the raspberry pi to the value passed to the dotnet program
+def detect_buttons(event_handler): #This function is called by Program.cs
+    pinToButtonId = { #Matches GPIO pins on the Pi to the value passed to Program.cs
         2: 1,
         3: 2,
         4: 3,
@@ -19,9 +17,9 @@ def detect_buttons(event_handler):
     def buttonPressed(id):
         event_handler.OnPressDetected(id)
 
-    for pin in pinToButtonId.keys(): #Foreach pin
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup pin on pi
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=buttonPressed(pinToButtonId[pin]), bouncetime=200) #If pin falls (buttons pressed) call buttonPressed function
+    for pin, id in pinToButtonId.items(): #Foreach pin
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup pin on Pi
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=buttonPressed(id), bouncetime=200) #If pin falls (buttons pressed) call buttonPressed function
 
     try:
         while True: #Let program keep running
