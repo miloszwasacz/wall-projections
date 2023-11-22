2e54e3b207d2795737449b12663e81985f471ab0
Daniel Diamand <daniel@diamand.org>
2023-11-22 12:21:06 +0000

    Fixed python bug, added on __name__ == "__main__" case

--- WallProjections/Scripts/main.py
+++ WallProjections/Scripts/main.py
@@ -14,15 +14,23 @@ def detect_buttons(event_handler):  # This function is called by Program.cs
         17: 4
     }
 
-    def buttonPressed(id):
+    def buttonPressed(pin):
+        id = pinToButtonId[pin]
+        print("button press", id)
         event_handler.OnPressDetected(id)
 
     for pin, id in pinToButtonId.items():  # Foreach pin
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Setup pin on Pi
-        GPIO.add_event_detect(pin, GPIO.FALLING, callback=buttonPressed(id), bouncetime=200)  # If pin falls (buttons pressed) call buttonPressed function
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=buttonPressed, bouncetime=200)  # If pin falls (buttons pressed) call buttonPressed function
 
     try:
         while True:  # Let program keep running
             time.sleep(0.1)
     except KeyboardInterrupt:
         GPIO.cleanup()
+
+if __name__ == "__main__":
+    class EventHandler:
+        def OnPressDetected(self, id):
+            print("button pressed", id)
+    detect_buttons(EventHandler())
