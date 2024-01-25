9ad098cdda30fc7d2bac3dd9f27b1be9a5b0a600
Sonny <ed22699@bristol.ac.uk>
2024-01-25 10:43:24 +0000

    Make single circle go to specific coordinates

--- WallProjections/ViewModels/HotspotViewModel.cs
+++ WallProjections/ViewModels/HotspotViewModel.cs
@@ -1,8 +1,44 @@
+using System;
+using System.Collections.Generic;
+using ReactiveUI;
+using WallProjections.Models;
 using WallProjections.ViewModels.Interfaces;
+using WallProjections.Models.Interfaces;
 
 namespace WallProjections.ViewModels;
 
 public class HotspotViewModel : ViewModelBase, IHotspotViewModel
 {
+    private List<Coord> _hotspots;
+    private readonly IConfig? _config;
     
+    public HotspotViewModel()
+    {
+        _config = new Config(
+            hotspots: new List<Hotspot>
+            {
+                new(id: 1, x: 100, y: 100, r: 50),
+                new(id: 2, x: 300, y: 200, r: 100)
+            });
+        _hotspots = new List<Coord>();
+        GetFirstHotspot();
+    }
+    
+    public List<Coord> Coordinates
+    {
+        get => _hotspots;
+        private set
+        {
+            this.RaiseAndSetIfChanged(ref _hotspots, value);
+        }
+    }
+
+    private void GetFirstHotspot()
+    {
+        var hotspot = _config?.GetHotspot(1);
+        if (hotspot is not null)
+        {
+            _hotspots.Add(hotspot.Position);
+        }
+    }
 }
--- WallProjections/ViewModels/Interfaces/IHotspotViewModel.cs
+++ WallProjections/ViewModels/Interfaces/IHotspotViewModel.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using WallProjections.Models;
+
 namespace WallProjections.ViewModels.Interfaces;
 
 public interface IHotspotViewModel
 {
-    
+    public List<Coord> Coordinates { get; }
 }
