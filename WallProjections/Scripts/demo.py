be0c4fee0f4d7d1e1ad5797997611af0ebd35f74
Daniel Diamand <daniel@diamand.org>
2024-02-12 17:32:43 +0000

    Add simple convertion from mediapipes data to camera space

--- WallProjections/Scripts/demo.py
+++ WallProjections/Scripts/demo.py
@@ -6,17 +6,11 @@ from cv2 import aruco
 from typing import Dict, Tuple
 
 
-hotspots: list[Hotspot] = []
-"""The global list of hotspots."""
+def normalizeToPixel(coord, width, height):
+    return (int(coord.x*width), int(coord.y*height))
 
-def transformFingertips(fingertips, matrix):
-    newFingertips = []
-    for fingertip in fingertips:
-        newFingertips.append(cv2.warpPerspective(np.array(fingertip), matrix, (1080,1920)))
-    
-    return newFingertips
 
-def run2(event_listener: EventListener, transformMatrix) -> None:  # This function is called by Program.cs
+def run2() -> None:  # This function is called by Program.cs
     """
     Captures video and runs the hand-detection model to handle the hotspots.
 
@@ -41,7 +35,6 @@ def run2(event_listener: EventListener, transformMatrix) -> None:  # This functi
         if not supported:
             logging.warning(f"Property id {prop_id} is not supported by video capture backend {VIDEO_CAPTURE_BACKEND}.")
 
-    hotspots = [Hotspot(0, 0.5, 0.5, event_listener), Hotspot(1, 0.8, 0.8, event_listener)]
 
     logging.info("Initialisation done.")
 
@@ -61,34 +54,12 @@ def run2(event_listener: EventListener, transformMatrix) -> None:  # This functi
 
         if hasattr(model_output, "multi_hand_landmarks") and model_output.multi_hand_landmarks is not None:
             # update hotspots
-            fingertip_coords = [landmarks.landmark[i] for i in FINGERTIP_INDICES for landmarks in
+            fingertip_coords = [landmarks.landmark[8] for landmarks in
                                 model_output.multi_hand_landmarks]
+            index_coord = normalizeToPixel(fingertip_coords[0], camera_width, camera_height)
+            cv2.circle(video_capture_img_rgb, index_coord, 3, (255,255,255), 2)
 
-            transformedFingertips = transformFingertips(fingertip_coords, transformMatrix)
-            for hotspot in hotspots:
-                hotspot_just_activated = hotspot.update(transformedFingertips)
-
-                if hotspot_just_activated:
-                    # make sure there's no other active hotspots
-                    for other_hotspot in hotspots:
-                        if other_hotspot == hotspot:
-                            continue
-                        other_hotspot.deactivate()
-
-                    event_listener.OnPressDetected(hotspot.id)
-
-            # draw hand landmarks
-            for landmarks in model_output.multi_hand_landmarks:
-                mp.solutions.drawing_utils.draw_landmarks(video_capture_img, landmarks,
-                                                          connections=mp.solutions.hands.HAND_CONNECTIONS)
-
-        # draw hotspot
-        image=np.full((1080,1920), 0,np.uint8) #generate empty background
-        # draw hotspot
-        for hotspot in hotspots:
-            hotspot.draw(image)
-
-        cv2.imshow("Projected Hotspots", image)
+        cv2.imshow("Calibration", video_capture_img_rgb)
 
         # development key inputs
         key = chr(cv2.waitKey(1) & 0xFF).lower()
@@ -110,26 +81,5 @@ def media_finished() -> None:
 
 
 if __name__ == "__main__":
-    #generate an example projectCoords dictionary
-    projectedCoords = {}
-    k = 0
-    for i in range(6):
-        for j  in range(12):
-            projectedCoords[k] = (25+(150*i), 25+(150*j))
-            k = k+1 
-    
-    arucoDict = aruco.getPredefinedDictionary(aruco.DICT_7X7_100)
-
-    cv2.namedWindow("Calibration", cv2.WINDOW_FULLSCREEN)
-    cv2.imshow("Calibration", drawArucos(projectedCoords, arucoDict))
-    cv2.waitKey(800)
-
-    cameraCoords = detectArucos(takePhoto(), arucoDict, displayResults=False)
-
-    camera2ProjectorMatrix = getTransformationMatrix(cameraCoords, projectedCoords)
-
-    class MyEventListener(EventListener):
-        def OnPressDetected(self, hotspot_id):
-            print(f"Hotspot {hotspot_id} activated.")
-
-    run2(MyEventListener(), camera2ProjectorMatrix)
+    tMatrix = example()
+    run2()
