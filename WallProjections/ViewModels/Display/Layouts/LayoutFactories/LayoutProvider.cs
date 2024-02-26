a2595f6e2a01f2a92b6c041b5c30e8f5ddf0cdb5
Thomas Parr <github.us8r1@tpalias.com>
2024-02-26 22:34:58 +0000

    Add auto discover code for layout factories

--- WallProjections/ViewModels/Display/Layouts/LayoutFactories/LayoutProvider.cs
+++ WallProjections/ViewModels/Display/Layouts/LayoutFactories/LayoutProvider.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using WallProjections.Models.Interfaces;
 using WallProjections.ViewModels.Display.Layouts;
 using WallProjections.ViewModels.Interfaces;
@@ -8,12 +10,30 @@ namespace WallProjections.Models.LayoutFactories;
 
 public class LayoutProvider : ILayoutProvider
 {
-    private readonly IReadOnlyCollection<ILayoutFactory> _layoutFactories
-        = new List<ILayoutFactory>
-        {
-            new DescriptionViewModelFactory(),
-            new VideoPlusDescriptionViewModelFactory()
-        };
+    /// <summary>
+    /// Finds all <see cref="ILayoutFactory">Layout Factories</see> that can make layouts automatically.
+    /// </summary>
+    public LayoutProvider()
+    {
+        // From: https://stackoverflow.com/questions/67079586/get-all-classes-that-implement-an-interface-and-call-a-function-in-net-core
+        _layoutFactories = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => GetType().Namespace.Equals(p.Namespace))
+            .Where(p => typeof(ILayoutFactory).IsAssignableFrom(p) && p.IsClass)
+            .Select(p => (ILayoutFactory)Activator.CreateInstance(p));
+
+    }
+
+    /// <summary>
+    /// Creates <see cref="LayoutProvider"/> with passed in <see cref="ILayoutFactory">Layout Factories</see>
+    /// </summary>
+    /// <param name="layoutFactories">Collection of <see cref="ILayoutFactory"/></param>
+    public LayoutProvider(IEnumerable<ILayoutFactory> layoutFactories)
+    {
+        _layoutFactories = layoutFactories;
+    }
+
+    private readonly IEnumerable<ILayoutFactory> _layoutFactories;
 
     public ILayout GetLayout(IViewModelProvider vmProvider, Hotspot.Media hotspot)
     {
