7835d6d80a730c4a819e32647b33f56170a62098
Daniel Diamand <daniel@diamand.org>
2024-03-03 11:37:52 +0000

    Add commented up consts and added name == main

--- WallProjections/Scripts/Internal/aruco_generator.py
+++ WallProjections/Scripts/Internal/aruco_generator.py
@@ -1,31 +1,47 @@
 import os.path
 from cv2 import aruco, imwrite
 
-NUMBER = 100  # max ArUco ID to gen
-DICT = aruco.getPredefinedDictionary(aruco.DICT_7X7_100)  # ArUco dict used
-ARUCO_SIZE = 100  # size of each ArUco in pixels
-ARUCO_BORDER = 1  # size of the border in "squares"
-FOLDER = os.path.abspath(__file__ + "/../../../Assets/ArUcos")  # folder for generated ArUcos
+
+DICT = aruco.getPredefinedDictionary(aruco.DICT_7X7_100)
+"""The CV2 ArUco dictionary, from which we generate our images. 
+Defined here https://docs.opencv.org/3.4/d9/d6a/group__aruco.html#gac84398a9ed9dd01306592dd616c2c975."""
+
+ARUCO_COUNT = 100
+"""The number of ArUcos we generate. Must not exceed the size of DICT
+(DICT_7x7_100 has max size 100, DICT_4x4_50 has max size 50, etc)."""
+
+ARUCO_SIZE = 100 
+"""The size of each ArUco image in pixels"""
+
+ARUCO_BORDER = 1
+"""The size of each ArUco border in "squares"."""
+
+FOLDER = os.path.abspath(__file__ + "/../../../Assets/ArUcos")
+"""The path to the folder where ArUco images get stored"""
 
 
 def get_aruco_path(aruco_id):
     return FOLDER + os.path.normpath("/ArUco_" + str(aruco_id) + ".png")
 
+def generate_arucos():
+    # Check if any ArUco images missing from FOLDER
+    arucos_missing = False
+    for code in range(ARUCO_COUNT):
+        file = get_aruco_path(code)
+        if not (os.path.exists(file) and os.path.isfile(file)):
+            arucos_missing = True
+            break
 
-# Check if there are any ArUcos missing
-all_exist = True
-for code in range(NUMBER):
-    file = get_aruco_path(code)
-    if not (os.path.exists(file) and os.path.isfile(file)):
-        all_exist = False
-        break
+    #If any ArUco images are missing generate all of them
+    if arucos_missing:
+        os.makedirs(FOLDER, exist_ok=True)
+        for code in range(ARUCO_COUNT):
+            arucoImage = aruco.generateImageMarker(DICT, code, ARUCO_SIZE, borderBits=ARUCO_BORDER)  # fetch ArUco
+            imwrite(get_aruco_path(code), arucoImage)
+        print("Generated " + str(ARUCO_COUNT) + " ArUcos")
+    else:
+        print("All ArUcos already exist")
 
-if not all_exist:
-    os.makedirs(FOLDER, exist_ok=True)
-    for code in range(NUMBER):
-        arucoImage = aruco.generateImageMarker(DICT, code, ARUCO_SIZE, borderBits=ARUCO_BORDER)  # fetch ArUco
-        imwrite(get_aruco_path(code), arucoImage)
-    print("Generated " + str(NUMBER) + " ArUcos")
-
-else:
-    print("All ArUcos already exist")
+if __name__ == "__main__":
+    generate_arucos()
+        
