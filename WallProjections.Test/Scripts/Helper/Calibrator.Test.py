74ec37df95b3364fbcf178da5da3db2974924471
danield <daniel@diamand.org>
2024-04-08 12:35:23 +0100

    Add more helper functions and tidied up code a little

--- WallProjections.Test/Scripts/Helper/Calibrator.Test.py
+++ WallProjections.Test/Scripts/Helper/Calibrator.Test.py
@@ -1,8 +1,20 @@
 import unittest
 import numpy as np
+import cv2
+from cv2 import aruco
 from WallProjections.Scripts.Helper.Calibrator import Calibrator
 
 TOLERANCE = 0.001
+DICT1 = {
+            1: (7.0, 1.0),
+            2: (5.3, 7.9),
+            3: (8.5, 7.9),
+            4: (3.2, 2.2)
+        }
+
+
+def as_numpy_dict(dictionary: dict[int, tuple[float, float]]) -> dict[int, tuple[np.float32, np.float32]]:
+    return {key: (np.float32(value[0]), np.float32(value[1])) for key, value in dictionary.items()}
 
 
 def matrix_same(a: np.ndarray, b: np.ndarray, tolerance=TOLERANCE) -> bool:
@@ -10,20 +22,15 @@ def matrix_same(a: np.ndarray, b: np.ndarray, tolerance=TOLERANCE) -> bool:
     return np.allclose(a, b, atol=tolerance)
 
 
-def matrix_identity(a: np.ndarray, tolerance=TOLERANCE) -> bool:
+def is_matrix_identity(a: np.ndarray, tolerance=TOLERANCE) -> bool:
     # Returns true if matrix A is the identity matrix, within tolerance.
     return matrix_same(a, np.identity(3))
-class MyTestCase(unittest.TestCase):
+
+
+class GetTransformTests(unittest.TestCase):
     def identical_points_return_identity(self):
-        test_dict1 = {
-            1: (1.0, 1.0),
-            2: (3.0, 3.9),
-            3: (131.1, 62.1),
-            4: (11.1, 89.1)
-        }
-        test_dict2 = {key: (np.float32(value[0]), np.float32(value[1])) for key, value in test_dict1.items()}
-        t_matrix = Calibrator._get_transformation_matrix(test_dict2, test_dict1)
-        self.assertTrue(matrix_identity(t_matrix))
+        t_matrix = Calibrator._get_transformation_matrix(as_numpy_dict(DICT1), DICT1)
+        self.assertTrue(is_matrix_identity(t_matrix))
 
 
 if __name__ == '__main__':
