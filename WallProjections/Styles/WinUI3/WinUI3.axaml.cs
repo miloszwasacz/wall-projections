62fa055a606df213cd404410f2bfb2ba376b9d60
miloszwasacz <wasacz.dev@gmail.com>
2024-04-19 18:53:42 +0100

    Add base values for WinUI3 theme

---
+++ WallProjections/Styles/WinUI3/WinUI3.axaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace WallProjections.Styles.WinUI3;
+
+// ReSharper disable once InconsistentNaming
+public class WinUI3 : Avalonia.Styling.Styles
+{
+    public WinUI3(IServiceProvider? sp = null)
+    {
+        AvaloniaXamlLoader.Load(sp, this);
+    }
+}
