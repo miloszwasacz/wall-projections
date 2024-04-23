a186b2a66400fdedde62beb45c5d776ec8871d8d
Rolodophone <rolodophone@gmail.com>
2024-04-23 15:56:09 +0100

    VideoCapture tests (1 test failing for some reason)

--- WallProjections/Scripts/Test/test_VideoCapture.py
+++ WallProjections/Scripts/Test/test_VideoCapture.py
@@ -1,9 +1,72 @@
+import time
 import unittest
 
+from Helper.VideoCapture import VideoCapture
 
-class VideoCapture(unittest.TestCase): # group tests in classes
-    def test_example(self):  # tests must be called test_something
-        self.assertTrue(True)
+
+class TestVideoCapture(unittest.TestCase):
+    def test_typical_lifecycle(self):
+        self.vidcap = VideoCapture(target="Assets/test_video.mp4")
+        self.vidcap.start()
+        time.sleep(0.1)
+        frame = self.vidcap.get_current_frame()
+        self.assertTrue(frame is not None)
+        time.sleep(0.1)
+        self.vidcap.stop()
+        self.assertRaises(RuntimeError, self.vidcap.get_current_frame)
+
+    def test_start(self):
+        self.vidcap = VideoCapture(target="Assets/test_video.mp4")
+        self.vidcap.start()
+        frame = self.vidcap.get_current_frame()
+        self.assertTrue(frame is not None)
+        self.vidcap.stop()
+
+    def test_stop(self):
+        self.vidcap = VideoCapture(target="Assets/test_video.mp4")
+        self.vidcap.start()
+        self.vidcap.stop()
+        self.assertRaises(RuntimeError, self.vidcap.get_current_frame)
+
+    def test_start_twice(self):
+        self.vidcap = VideoCapture(target="Assets/test_video.mp4")
+        self.vidcap.start()
+        self.assertRaises(RuntimeError, self.vidcap.start)
+        self.vidcap.stop()
+
+    def test_stop_twice(self):
+        self.vidcap = VideoCapture(target="Assets/test_video.mp4")
+        self.vidcap.start()
+        self.vidcap.stop()
+        self.assertRaises(RuntimeError, self.vidcap.stop)
+
+    def test_multiple_instances(self):
+        vidcap1 = VideoCapture(target="Assets/test_video.mp4")
+        vidcap2 = VideoCapture(target="Assets/test_video.mp4")
+        vidcap1.start()
+        try:
+            vidcap2.start()  # may or may not work depending on video capture target
+        except RuntimeError:
+            pass
+        frame1 = vidcap1.get_current_frame()
+        self.assertTrue(frame1 is not None)
+        vidcap1.stop()
+        vidcap2.start()
+        try:
+            vidcap1.start()  # may or may not work depending on video capture target
+        except RuntimeError:
+            pass
+        frame2 = vidcap2.get_current_frame()
+        self.assertTrue(frame2 is not None)
+        vidcap2.stop()
+
+    def test_take_photo(self):
+        frame = VideoCapture.take_photo(target="Assets/test_video.mp4")
+        self.assertTrue(frame is not None)
+
+    def tearDown(self):  # this will get called after every test
+        if hasattr(self, 'vidcap') and self.vidcap._video_capture_thread.is_alive():
+            self.vidcap.stop()
 
 
 if __name__ == '__main__':
