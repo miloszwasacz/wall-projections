ffdb4ebf83beeb8b89f3b49774cd0d6e800bb172
Sonny <ed22699@bristol.ac.uk>
2024-04-24 16:01:09 +0100

    Change NotImplementedException to NotSupportedException
    Change to NotSupportedException and changed runtime for activation to 2 seconds

--- WallProjections/Views/Converters/StateConverter.cs
+++ WallProjections/Views/Converters/StateConverter.cs
@@ -27,7 +27,7 @@ public class StateConverter : IValueConverter
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
 
